b029f16245093a55c6ecf3c4a1c0db3ddba559cd
alvaro <tireempuje@gmail.com>
2023-08-07 00:28:58 +0200

    Added (buggy) function to check if two segments intersect.

--- functions.py
+++ functions.py
@@ -9,3 +9,35 @@ def get_coordinates():
         coordinates[i] = float(coordinates[i])
 
     return coordinates
+
+
+def line_parameters(point_1, point_2):
+
+    m = (point_2[1] - point_1[1]) / (point_2[0] - point_1[0])
+    n = point_1[1] - point_1[0] * m
+
+    return m, n
+
+
+def point_in_line(point_coordinates, segment_vertex_1, segment_vertex_2):
+
+    m, n = line_parameters(segment_vertex_1, segment_vertex_2)
+
+    if (point_coordinates[1] == m * point_coordinates[0] + n) and \
+            ((segment_vertex_1[0] < point_coordinates[0] < segment_vertex_2[0]) or
+             (segment_vertex_2[0] < point_coordinates[0] < segment_vertex_1[0])):
+        return True
+    return False
+
+
+def line_with_line(segment_1_vertex_1, segment_1_vertex_2, segment_2_vertex_1, segment_2_vertex_2):
+
+    m1, n1 = line_parameters(segment_1_vertex_1, segment_1_vertex_2)
+
+    if (segment_2_vertex_1[1] < (m1 * segment_2_vertex_1[0] + n1)
+            and segment_2_vertex_2[1] > (m1 * segment_2_vertex_2[0] + n1)) \
+        or (segment_2_vertex_2[1] < (m1 * segment_2_vertex_2[0] + n1)
+            and segment_2_vertex_1[1] > (m1 * segment_2_vertex_1[0] + n1)):
+
+        return True
+    return False
--- main.py
+++ main.py
@@ -1,15 +1,44 @@
-import shapes, isinside, functions
+import shapes
+import functions
 
-print("Enter the coordinates of the first point of the segment")
-vertex_1_segment = functions.get_coordinates()
-print("Enter the coordinates of the second point of the segment")
-vertex_2_segment = functions.get_coordinates()
+choice = ''
 
-segment = shapes.Segment(vertex_1_segment, vertex_2_segment)
+while choice not in (1, 2):
+    choice = int(input("""Select an option
+    1 - Point in line.
+    2 - Line cross with line
+    """))
 
-print("Enter the coordinates of the point")
-point = shapes.Point(functions.get_coordinates())
+if choice == 1:
+    print("Enter the coordinates of the first point of the segment")
+    vertex_1_segment = functions.get_coordinates()
+    print("Enter the coordinates of the second point of the segment")
+    vertex_2_segment = functions.get_coordinates()
 
-print(point.coordinates, segment.vertex_1, segment.vertex_2)
+    segment = shapes.Segment(vertex_1_segment, vertex_2_segment)
 
-print(isinside.point_in_line(point.coordinates, segment.vertex_1, segment.vertex_2))
+    print("Enter the coordinates of the point")
+    point = shapes.Point(functions.get_coordinates())
+
+    print(segment.vertex_1, segment.vertex_2, point.coordinates)
+
+    print(functions.point_in_line(point.coordinates, segment.vertex_1, segment.vertex_2))
+
+elif choice == 2:
+    print("Enter the coordinates of the first point of the first segment")
+    vertex_1_segment_1 = functions.get_coordinates()
+    print("Enter the coordinates of the second point of the first segment")
+    vertex_2_segment_1 = functions.get_coordinates()
+
+    segment_1 = shapes.Segment(vertex_1_segment_1, vertex_2_segment_1)
+
+    print("Enter the coordinates of the first point of the second segment")
+    vertex_1_segment_2 = functions.get_coordinates()
+    print("Enter the coordinates of the second point of the second segment")
+    vertex_2_segment_2 = functions.get_coordinates()
+
+    segment_2 = shapes.Segment(vertex_1_segment_2, vertex_2_segment_2)
+
+    print(segment_1.vertex_1, segment_1.vertex_2, segment_2.vertex_1, segment_2.vertex_2)
+
+    print(functions.line_with_line(segment_1.vertex_1, segment_1.vertex_2, segment_2.vertex_1, segment_2.vertex_2))
